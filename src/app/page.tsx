6c4a21f46a7631792b37edcd16f026782ea28432
Hafeez Abubakar <89017864+ameer-Sharif15@users.noreply.github.com>
2025-02-26 13:03:15 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,16 @@ export default function Home() {
   return (
     <div className="flex flex-col bg-white">
       <HeroSection />
-      {/* <div className="flex flex-col z-10 bg-white">
+<div className="flex overflow-hidden flex-col z-10
+      {/* 
+<div className="flex flex-col z-10 bg-white">
       <ServicesSection />
       <ProcessSection />
       <Footer />
       </div> */}
+
       <AdditionalInfo />
+</div>
     </div>
   );
 }
